331cdb40eaf4b2fb7f1e5d455f524cd8592a157d
jfederico <jesus@123it.ca>
2011-10-06 12:06:02 -0400

    2.1_8.1.4: Updated version files. Signed-off-by: jfederico <jesus@123it.ca>

--- moodle21/recordingsbn/version.php
+++ moodle21/recordingsbn/version.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * View and administrate BigBlueButton playback recordings
  *
@@ -11,10 +10,11 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
  */
 
-
 defined('MOODLE_INTERNAL') || die();
 
 $module->version   = 2011100400;      // The current module version (Date: YYYYMMDDXX)
 $module->requires  = 2010080300;      // Requires this Moodle version
 $module->cron      = 0;               // Period for cron to check this module (secs)
 $module->component = 'mod_recordingsbn'; // To check on upgrade, that module sits in correct place
+$module->maturity = MATURITY_BETA;      // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
+$module->release  = ''; 
