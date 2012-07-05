54524d0087b7d840ce23a6ce813e79fa45fd74c7
jfederico <jesus@123it.ca>
2012-07-05 17:13:58 -0400

    Updated help string Signed-off-by: jfederico <jesus@123it.ca>

--- version.php
+++ version.php
@@ -13,11 +13,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$module->version  = 2012070500;
+$module->version  = 2012070505;
 $module->requires = 2010112400;
 $module->cron     = 0;
 $module->component = 'mod_bigbluebuttonbn';
 $module->maturity = MATURITY_BETA;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
 $module->release  = '1.0.8'; 
-
-?>
