83042b85700daf52c122baa5ae5ecb5c3c65e203
jfederico <jesus@123it.ca>
2012-07-12 11:17:20 -0400

    v1.0.8-b2: Increased version number Signed-off-by: jfederico <jesus@123it.ca>

--- version.php
+++ version.php
@@ -13,7 +13,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$module->version  = 2012070900;
+$module->version  = 2012070905;
 $module->requires = 2010112400;
 $module->cron     = 0;
 $module->component = 'mod_bigbluebuttonbn';
