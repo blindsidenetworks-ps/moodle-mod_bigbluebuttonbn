3ee36f5bf9c1b0c415d7254bc313ceb2b7c83652
jfederico <jesus@123it.ca>
2012-12-06 13:58:10 -0500

    v1.0.8-rc2: Updated version serial Signed-off-by: jfederico <jesus@123it.ca>

--- version.php
+++ version.php
@@ -13,7 +13,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$module->version  = 2012110100;
+$module->version  = 2012120100;
 $module->requires = 2010112400;
 $module->cron     = 0;
 $module->component = 'mod_bigbluebuttonbn';
