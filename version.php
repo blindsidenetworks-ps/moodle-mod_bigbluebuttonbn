33fd67c0bfee320b9460544560df2d9acdb5e052
jfederico <jesus@123it.ca>
2012-12-06 15:47:00 -0500

    v1.0.8: Updated maturity tag. Release rc2 was promoted to stable Signed-off-by: jfederico <jesus@123it.ca>

--- version.php
+++ version.php
@@ -17,5 +17,5 @@ $module->version  = 2012120100;
 $module->requires = 2010112400;
 $module->cron     = 0;
 $module->component = 'mod_bigbluebuttonbn';
-$module->maturity = MATURITY_RC;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
+$module->maturity = MATURITY_STABLE;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
 $module->release  = '1.0.8'; 
