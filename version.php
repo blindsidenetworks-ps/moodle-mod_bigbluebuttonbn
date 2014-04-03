431903dab6b46e7af5032d6a96f99c8323c1b296
Jesus Federico <jesus@123it.ca>
2014-04-03 13:12:32 -0400

    Updated version tag

--- version.php
+++ version.php
@@ -13,7 +13,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$module->version  = 2013110103;
+$module->version  = 2013110104;
 $module->requires = 2010112400;
 $module->cron     = 0;
 $module->component = 'mod_bigbluebuttonbn';
