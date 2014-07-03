fc2905de2c7be3ff446f46bb06cba1fbadab2af2
Jesus Federico <jesus@123it.ca>
2014-07-03 13:27:53 -0400

    v1.0.11-rc1: Updated version tag

--- version.php
+++ version.php
@@ -13,9 +13,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2014070100;
+$plugin->version  = 2014070101;
 $plugin->requires = 2013101800;
 $plugin->cron     = 0;
 $plugin->component = 'moodle-mod_bigbluebuttonbn';
 $plugin->maturity = MATURITY_RC;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
-$plugin->release  = '1.0.10';
+$plugin->release  = '1.0.11';
