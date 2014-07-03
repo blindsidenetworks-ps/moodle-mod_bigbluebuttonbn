ff9d7d46bee5573b37cb13d33b93644ca34c758f
Jesus Federico <jesus@123it.ca>
2014-07-03 11:24:52 -0400

    v1.0.10-rc1: Removed context menu from waiting for moderator page and updated version tag

--- version.php
+++ version.php
@@ -7,15 +7,15 @@
  *    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)    
  * 
  * @package   mod_bigbluebuttonbn
- * @copyright 2010-2013 Blindside Networks 
+ * @copyright 2010-2014 Blindside Networks Inc.
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
 
-$module->version  = 2014050100;
+$module->version  = 2014070100;
 $module->requires = 2010112400;
 $module->cron     = 0;
 $module->component = 'mod_bigbluebuttonbn';
-$module->maturity = MATURITY_BETA;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
+$module->maturity = MATURITY_RC;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
 $module->release  = '1.0.10';
