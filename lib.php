3bb9ea5397e9d8c1b602e3bef0ca77e021cff1a2
Jesus Federico <jesus@123it.ca>
2015-04-23 16:35:52 -0400

    v2.0.0: Activated mod intro

--- lib.php
+++ lib.php
@@ -19,12 +19,13 @@ function bigbluebuttonbn_supports($feature) {
         case FEATURE_GROUPS:                  return true;
         case FEATURE_GROUPINGS:               return true;
         case FEATURE_GROUPMEMBERSONLY:        return true;
-        case FEATURE_MOD_INTRO:               return false;
+        case FEATURE_MOD_INTRO:               return true;
+        case FEATURE_BACKUP_MOODLE2:          return true;
         case FEATURE_COMPLETION_TRACKS_VIEWS: return true;
         case FEATURE_GRADE_HAS_GRADE:         return false;
         case FEATURE_GRADE_OUTCOMES:          return false;
+        case FEATURE_SHOW_DESCRIPTION:        return true;
         // case FEATURE_MOD_ARCHETYPE:           return MOD_ARCHETYPE_RESOURCE;
-        case FEATURE_BACKUP_MOODLE2:          return true;
 
         default: return null;
     }
