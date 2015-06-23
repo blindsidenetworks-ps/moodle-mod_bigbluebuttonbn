d786d8f6714f66353e636aa11f930dc358d0bf47
Jesus Federico <jesus@123it.ca>
2015-06-23 16:42:31 -0400

    Updated version tag in preparation for a new release

--- version.php
+++ version.php
@@ -11,16 +11,16 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-#$module->version = 2014101007;
+#$module->version = 2014101008;
 #$module->requires = 2010112400;
 #$module->cron = 0;
 #$module->component = 'mod_bigbluebuttonbn';
 #$module->maturity = MATURITY_STABLE;    // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
-#$module->release = '1.0.11';
+#$module->release = '1.1.2';
 
-$plugin->version  = 2014101007;
+$plugin->version  = 2014101008;
 $plugin->requires = 2013111800;
 $plugin->cron     = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
 $plugin->maturity = MATURITY_STABLE;    // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
-$plugin->release  = '1.0.11';
+$plugin->release  = '1.1.2';
