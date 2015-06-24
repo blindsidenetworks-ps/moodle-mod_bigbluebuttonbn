d560e3e0bb2a3e9a27ac8d6da467e859eea0ded1
Jesus Federico <jesus@123it.ca>
2015-06-24 18:18:37 -0400

    v1.1.0: Updated version tag

--- version.php
+++ version.php
@@ -11,16 +11,16 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-#$module->version = 2015062100;
+#$module->version = 2015062101;
 #$module->requires = 2010112400;
 #$module->cron = 0;
 #$module->component = 'mod_bigbluebuttonbn';
 #$module->maturity = MATURITY_STABLE;    // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
-#$module->release = '1.1.0';
+#$module->release = '1.1.1';
 
-$plugin->version  = 2015062100;
+$plugin->version  = 2015062101;
 $plugin->requires = 2013111800;
 $plugin->cron     = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
 $plugin->maturity = MATURITY_STABLE;    // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
-$plugin->release  = '1.1.0';
+$plugin->release  = '1.1.1';
