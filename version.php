31b29fd7104f063ac54e86c3124ecd2f7225c69f
jfederico <jesus@123it.ca>
2015-11-20 12:12:21 -0500

    v2.0.0: Updated version tag in preparation for release

--- version.php
+++ version.php
@@ -11,9 +11,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2015080605;
+$plugin->version  = 2015080606;
 $plugin->requires = 2013111800;
 $plugin->cron     = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
-$plugin->maturity = MATURITY_RC;    // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
+$plugin->maturity = MATURITY_STABLE;    // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
 $plugin->release  = '2.0.0';
