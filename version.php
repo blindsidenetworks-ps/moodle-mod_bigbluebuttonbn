53bf94684fe826be6e865cba79aa1169e021f4e1
jfederico <jesus@123it.ca>
2016-05-20 09:45:56 -0400

    v2.1.0: Updated version tag in preparation for beta release (2016051900)

--- version.php
+++ version.php
@@ -5,15 +5,15 @@
  * @package   mod_bigbluebuttonbn
  * @author    Fred Dixon  (ffdixon [at] blindsidenetworks [dt] com)
  * @author    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
- * @copyright 2010-2015 Blindside Networks Inc.
+ * @copyright 2010-2016 Blindside Networks Inc.
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2016011305;
+$plugin->version  = 2016051900;
 $plugin->requires = 2013111800;
 $plugin->cron     = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_BETA;
 $plugin->release  = '2.1.0';
