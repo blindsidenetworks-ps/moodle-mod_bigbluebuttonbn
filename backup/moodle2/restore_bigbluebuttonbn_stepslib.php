63802f7509afc92d096c55dfc3970a458dea331c
jfederico <jesus@123it.ca>
2017-02-07 20:30:46 +0000

    v2.1.6: Fixed issue with references to recordings and imported links not being properly restored when using the option import from backups

--- backup/moodle2/restore_bigbluebuttonbn_stepslib.php
+++ backup/moodle2/restore_bigbluebuttonbn_stepslib.php
@@ -37,8 +37,9 @@ class restore_bigbluebuttonbn_activity_structure_step extends restore_activity_s
         $userinfo = $this->get_setting_value('userinfo');
 
         $paths[] = new restore_path_element('bigbluebuttonbn', '/activity/bigbluebuttonbn');
+        $paths[] = new restore_path_element('bigbluebuttonbn_logs', '/activity/bigbluebuttonbn/logs/log');
+
         if ($userinfo) {
-            $paths[] = new restore_path_element('bigbluebuttonbn_logs', '/activity/bigbluebuttonbn/logs/log');
         }
 
         // Return the paths wrapped into standard activity structure
@@ -66,14 +67,11 @@ class restore_bigbluebuttonbn_activity_structure_step extends restore_activity_s
         $data = (object)$data;
         $oldid = $data->id;
         // Apply modifications
-        $data->courseid = $this->get_mappingid('course', $data->courseid);
         $data->bigbluebuttonbnid = $this->get_new_parentid('bigbluebuttonbn');
         $data->userid = $this->get_mappingid('user', $data->userid);
         $data->timecreated = $this->apply_date_offset($data->timecreated);
 
-        // insert the bigbluebuttonbn_logs record
         $newitemid = $DB->insert_record('bigbluebuttonbn_logs', $data);
-        // immediately after inserting associated record, call this
         $this->set_mapping('bigbluebuttonbn_logs', $oldid, $newitemid); // because of decode
     }
 
