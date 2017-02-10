692be94c7931e0a3f4b4c333bcd9f4273a2f2d8f
jfederico <jesus@123it.ca>
2017-02-10 20:44:50 +0000

    v2.2-dev: Fixed issue in import_view affecting the new option for importing recordings from current course

--- import_view.php
+++ import_view.php
@@ -54,14 +54,14 @@ if( empty($options) ) {
     $output .= html_writer::tag('div', html_writer::select($options, 'import_recording_links_select', $selected));
 
     //get course recordings
-    if ($bbbsession['course']->id == $selected) {
-        $recordings = bigbluebuttonbn_get_recordings($selected, $bbbsession['bigbluebuttonbn']->id, false, bigbluebuttonbn_get_cfg_importrecordings_from_deleted_activities_enabled());
+    if ($course->id == $selected) {
+        $recordings = bigbluebuttonbn_get_recordings($selected, $bigbluebuttonbn->id, false, bigbluebuttonbn_get_cfg_importrecordings_from_deleted_activities_enabled());
     } else {
         $recordings = bigbluebuttonbn_get_recordings($selected, null, null, bigbluebuttonbn_get_cfg_importrecordings_from_deleted_activities_enabled());
     }
     if (!empty($recordings)) {
         //exclude the ones that are already imported
-        $recordings = bigbluebuttonbn_unset_existent_recordings_already_imported($bbbsession['course']->id, $bbbsession['bigbluebuttonbn']->id, $recordings);
+        $recordings = bigbluebuttonbn_unset_existent_recordings_already_imported($course->id, $bigbluebuttonbn->id, $recordings);
     }
     //store recordings (indexed) in a session variable
     $SESSION->bigbluebuttonbn_importrecordings = $recordings;
