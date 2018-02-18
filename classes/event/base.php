c84961fbe950ebaf14aaef4e5a6549ba24c6d397
jfederico <jesus@123it.ca>
2018-02-18 19:01:41 +0000

    v2.2.2: Fixed lint error and enabled travis build

--- classes/event/base.php
+++ classes/event/base.php
@@ -83,7 +83,7 @@ abstract class base extends \core\event\base {
           );
         $string = $this->description;
         foreach ($vars as $key => $value) {
-          $string = str_replace("##" . $key, $value, $string);
+            $string = str_replace("##" . $key, $value, $string);
         }
         return $string;
     }
