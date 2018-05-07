3aa1ae9512541493ae57cd52d9bcf64ab94a8f1b
jfederico <jesus@123it.ca>
2018-05-07 15:44:03 +0000

    v2.2.4: Bummped up tags for version release

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2017101011;
+$plugin->version = 2017101012;
 $plugin->requires = 2015111610;
 $plugin->cron = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.2.3';
+$plugin->release = '2.2.4';
