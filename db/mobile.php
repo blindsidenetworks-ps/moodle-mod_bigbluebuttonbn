6b0ada9f98bb6cfc020faac5b55868cfd700cba9
jfederico <jesus@123it.ca>
2018-09-11 21:56:28 +0000

    Added codebase for mobile

---
+++ db/mobile.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Mobile app definition for BigBlueButton.
+ *
+ * @package   mod_bigbluebuttonbn
+ * @copyright 2018 onwards, Blindside Networks Inc
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$addons = array(
+    "mod_bigbluebuttonbn" => array(
+        "handlers" => array( // Different places where the add-on will display content.
+            'coursebigbluebuttonbn' => array( // Handler unique name (can be anything)
+                'displaydata' => array(
+                    'title' => 'pluginname',
+                    'icon' => $CFG->wwwroot . '/mod/bigbluebuttonbn/pix/icon.gif',
+                    'class' => '',
+                ),
+                'delegate' => 'CoreCourseModuleDelegate', // Delegate (where to display the link to the add-on)
+                'method' => 'mobile_course_view' // Main function in \mod_bigbluebuttonbn\output\mobile
+            )
+        ),
+        'lang' => array(
+            array('pluginname', 'bigbluebuttonbn')
+        )
+    )
+);
