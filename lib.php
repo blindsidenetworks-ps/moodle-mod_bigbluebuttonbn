bb9958887e78547e9b605a1168b5001e5bf06af3
jfederico <jesus@123it.ca>
2018-10-30 20:26:03 +0000

    Remove duplicated code from lib

--- lib.php
+++ lib.php
@@ -743,35 +743,6 @@ function bigbluebuttonbn_check_updates_since(cm_info $cm, $from, $filter = array
     return $updates;
 }
 
-/**
- * This function receives a calendar event and returns the action associated with it, or null if there is none.
- *
- * This is used by block_myoverview in order to display the event appropriately. If null is returned then the event
- * is not displayed on the block.
- *
- * @param calendar_event $event
- * @param \core_calendar\action_factory $factory
- * @return \core_calendar\local\event\entities\action_interface|null
- */
-function mod_bigbluebuttonbn_core_calendar_provide_event_action(calendar_event $event,
-                                                       \core_calendar\action_factory $factory) {
-    $cm = get_fast_modinfo($event->courseid)->instances['bigbluebuttonbn'][$event->instance];
-
-    $completion = new \completion_info($cm->get_course());
-
-    $completiondata = $completion->get_data($cm, false);
-
-    if ($completiondata->completionstate != COMPLETION_INCOMPLETE) {
-        return null;
-    }
-
-    return $factory->create_instance(
-        get_string('view'),
-        new \mod_bigbluebuttonbn('/mod/bigbluebuttonbn/view.php', ['id' => $cm->id]),
-        1,
-        true
-    );
-}
 
 /**
  * Get icon mapping for font-awesome.
