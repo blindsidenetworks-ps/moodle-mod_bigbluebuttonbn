ea81511f795a6c8f7c5cb859ccc33e1ae4f2a18a
germanvitale <vitalegerman@gmail.com>
2018-11-30 02:34:04 -0300

    CONTRIB-7538: Code cleaning

--- bbb_view.php
+++ bbb_view.php
@@ -26,6 +26,8 @@
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/locallib.php');
 
+global $SESSION;
+
 $action = required_param('action', PARAM_TEXT);
 $id = optional_param('id', 0, PARAM_INT);
 $bn = optional_param('bn', 0, PARAM_INT);
@@ -47,44 +49,49 @@ $bigbluebuttonbn = $bbbviewinstance['bigbluebuttonbn'];
 $context = context_module::instance($cm->id);
 
 if ($timeline) {
-    // If the user came from timeline, the $bbbsession should be created here.
+    // If the user come from timeline, the $bbbsession should be created here.
     $bbbsession['course'] = $course;
     $bbbsession['coursename'] = $course->fullname;
     $bbbsession['cm'] = $cm;
     $bbbsession['bigbluebuttonbn'] = $bigbluebuttonbn;
     bigbluebuttonbn_view_bbbsession_set($context, $bbbsession);
 
-    // Check activity status.
-    $activitystatus = bigbluebuttonbn_view_get_activity_status($bbbsession);
-    // TODO.
-    /*if ($activitystatus == 'not_started') {
-        $actionable = false;
-    }
-    if ($activitystatus == 'ended') {
-        $actionable = false;
-    }*/
+    // Check status and set extra values.
+    bigbluebuttonbn_view_get_activity_status($bbbsession);
 
     // Validates if the BigBlueButton server is working.
     $serverversion = bigbluebuttonbn_get_server_version();
     if (is_null($serverversion)) {
-        $actionable = false;
-    } else {
-        $bbbsession['serverversion'] = (string) $serverversion;
-
-        // Operation URLs.
-        $bbbsession['bigbluebuttonbnURL'] = $CFG->wwwroot . '/mod/bigbluebuttonbn/view.php?id=' . $bbbsession['cm']->id;
-        $bbbsession['logoutURL'] = $CFG->wwwroot . '/mod/bigbluebuttonbn/bbb_view.php?action=logout&id='.$cm->id .
-            '&bn=' . $bbbsession['bigbluebuttonbn']->id;
-        $bbbsession['recordingReadyURL'] = $CFG->wwwroot . '/mod/bigbluebuttonbn/bbb_broker.php?action=recording_' .
-            'ready&bigbluebuttonbn=' . $bbbsession['bigbluebuttonbn']->id;
-        $bbbsession['meetingEventsURL'] = $CFG->wwwroot . '/mod/bigbluebuttonbn/bbb_broker.php?action=meeting' .
-            '_events&bigbluebuttonbn=' . $bbbsession['bigbluebuttonbn']->id;
-        $bbbsession['joinURL'] = $CFG->wwwroot . '/mod/bigbluebuttonbn/bbb_view.php?action=join&id=' . $cm->id .
-            '&bn=' . $bbbsession['bigbluebuttonbn']->id;
-        // Initialize session variable used across views.
-        // TODO check if this is need it.
-        $SESSION->bigbluebuttonbn_bbbsession = $bbbsession;
+        if ($bbbsession['administrator']) {
+            print_error('view_error_unable_join', 'bigbluebuttonbn',
+                $CFG->wwwroot.'/admin/settings.php?section=modsettingbigbluebuttonbn');
+            exit;
+        }
+        if ($bbbsession['moderator']) {
+            print_error('view_error_unable_join_teacher', 'bigbluebuttonbn',
+                $CFG->wwwroot.'/course/view.php?id='.$bigbluebuttonbn->course);
+            exit;
+        }
+        print_error('view_error_unable_join_student', 'bigbluebuttonbn',
+            $CFG->wwwroot.'/course/view.php?id='.$bigbluebuttonbn->course);
+        exit;
     }
+
+    $bbbsession['serverversion'] = (string) $serverversion;
+
+    // Operation URLs.
+    $bbbsession['bigbluebuttonbnURL'] = $CFG->wwwroot . '/mod/bigbluebuttonbn/view.php?id=' . $bbbsession['cm']->id;
+    // The user should be back to Dashboard.
+    $bbbsession['logoutURL'] = $CFG->wwwroot . '/my/';
+    $bbbsession['recordingReadyURL'] = $CFG->wwwroot . '/mod/bigbluebuttonbn/bbb_broker.php?action=recording_' .
+        'ready&bigbluebuttonbn=' . $bbbsession['bigbluebuttonbn']->id;
+    $bbbsession['meetingEventsURL'] = $CFG->wwwroot . '/mod/bigbluebuttonbn/bbb_broker.php?action=meeting' .
+        '_events&bigbluebuttonbn=' . $bbbsession['bigbluebuttonbn']->id;
+    $bbbsession['joinURL'] = $CFG->wwwroot . '/mod/bigbluebuttonbn/bbb_view.php?action=join&id=' . $cm->id .
+        '&bn=' . $bbbsession['bigbluebuttonbn']->id;
+    // Initialize session variable used across views.
+    $SESSION->bigbluebuttonbn_bbbsession = $bbbsession;
+
 } elseif (isset($SESSION) && isset($SESSION->bigbluebuttonbn_bbbsession)) {
     $bbbsession = $SESSION->bigbluebuttonbn_bbbsession;
 }
