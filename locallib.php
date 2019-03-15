467d6952acd40222e793290e2084cd92f725f139
germanvitale <vitalegerman@gmail.com>
2019-03-14 22:50:28 -0300

    CONTRIB-7644: Changes on clienttype validations

--- locallib.php
+++ locallib.php
@@ -3122,9 +3122,11 @@ function bigbluebuttonbn_view_bbbsession_set($context, &$bbbsession) {
     $bbbsession['originTag'] = 'moodle-mod_bigbluebuttonbn ('.get_config('mod_bigbluebuttonbn', 'version').')';
     $bbbsession['bnserver'] = bigbluebuttonbn_is_bn_server();
     // Setting for clienttype, assign flash if not enabled, or default if not editable.
-    $bbbsession['clienttype'] = \mod_bigbluebuttonbn\locallib\config::get('clienttype_default');
     $bbbsession['clienttype'] = BIGBLUEBUTTON_CLIENTTYPE_FLASH;
-    if (isset($bbbsession['bigbluebuttonbn']->clienttype)) {
+    if (\mod_bigbluebuttonbn\locallib\config::clienttype_enabled()) {
+        $bbbsession['clienttype'] = \mod_bigbluebuttonbn\locallib\config::get('clienttype_default');
+    }
+    if (\mod_bigbluebuttonbn\locallib\config::get('clienttype_editable') && isset($bbbsession['bigbluebuttonbn']->clienttype)) {
         $bbbsession['clienttype'] = $bbbsession['bigbluebuttonbn']->clienttype;
     }
 }
