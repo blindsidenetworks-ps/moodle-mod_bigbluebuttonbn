d8932d922982c59b1fa7a645137e389cef86782f
jfederico <jesus@123it.ca>
2019-05-16 17:59:08 +0000

    GIT-130: Fixed #130 for v2.2-stable

--- lang/en/bigbluebuttonbn.php
+++ lang/en/bigbluebuttonbn.php
@@ -225,9 +225,7 @@ $string['modulename'] = 'BigBlueButtonBN';
 $string['modulenameplural'] = 'BigBlueButtonBN';
 $string['modulename_help'] = 'BigBlueButtonBN lets you create from within Moodle links to real-time on-line classrooms using BigBlueButton, an open source web conferencing system for distance education.
 
-Using BigBlueButtonBN you can specify for the title, description, calendar entry (which gives a date range for joining the session), groups, and details about the recording of the on-line session.
-
-To view later recordings, add a RecordingsBN resource to this course.';
+Using BigBlueButtonBN you can specify for the title, description, calendar entry (which gives a date range for joining the session), groups, and details about the recording of the on-line session.';
 $string['modulename_link'] = 'BigBlueButtonBN/view';
 $string['starts_at'] = 'Starts';
 $string['started_at'] = 'Started';
@@ -325,6 +323,7 @@ $string['view_message_session_has_user'] = 'There is';
 $string['view_message_session_has_users'] = 'There are';
 $string['view_message_session_for'] = 'the session for';
 $string['view_message_times'] = 'times';
+$string['view_message_and'] = 'and';
 
 $string['view_message_room_closed'] = 'This room is closed.';
 $string['view_message_room_ready'] = 'This room is ready.';
