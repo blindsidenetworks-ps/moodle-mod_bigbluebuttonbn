0fd708b95bee5a6b25a8f6f08ad50f276f362341
jfederico <jesus@123it.ca>
2019-07-24 17:03:30 +0000

    v2.3-rc1: Bumped up version tags

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2019042003;
+$plugin->version = 2019042004;
 $plugin->requires = 2016120500;
 $plugin->cron = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
-$plugin->maturity = MATURITY_BETA;
-$plugin->release = '2.3-beta';
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '2.3-rc';
