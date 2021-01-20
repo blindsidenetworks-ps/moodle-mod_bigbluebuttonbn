933f50caef4f51cd325da65d8b4cd925a2714b5c
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2021-01-20 02:34:11 +0100

    bbb_ajax.php: declare READ_ONLY_SESSION
    Declare that calls to bbb_ajax.php only require a read-only session,
    which reduces session lock contention on pages calling making AJAX
    requests to this endpoint.
    
    See-also: MDL-58018

--- bbb_ajax.php
+++ bbb_ajax.php
@@ -23,6 +23,8 @@
  * @author    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
  */
 
+define('READ_ONLY_SESSION', true);
+
 require(__DIR__.'/../../config.php');
 require_once(__DIR__.'/locallib.php');
 require_once(__DIR__.'/brokerlib.php');
