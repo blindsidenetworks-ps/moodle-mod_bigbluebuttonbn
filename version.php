41e7eab194180e088ad6fac7f3f58c30f6b041ba
jfederico <jesus@123it.ca>
2021-01-27 18:02:51 +0000

    bumped up version tag

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2019042009;
+$plugin->version = 2019042010;
 $plugin->requires = 2016120500;
 $plugin->cron = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.3.4';
+$plugin->release = '2.3.5';
