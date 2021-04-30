d010fb1910796364a4b94ae888a9f98bcf99bf9f
jfederico <jesus@123it.ca>
2021-04-30 20:10:54 +0000

    v2.4-rc.2: release

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2019101005;
+$plugin->version = 2019101006;
 $plugin->requires = 2016120500;
 $plugin->cron = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '2.4-rc';
+$plugin->release = '2.4-rc.2';
