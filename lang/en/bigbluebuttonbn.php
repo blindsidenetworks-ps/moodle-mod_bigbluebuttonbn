d5dfef537a5a41a2e0a461d0e24eb1023eb843cc
Laurent David <lmedavid@gmail.com>
2021-05-26 13:47:01 +0200

    CONTRIB-8435: Welcome default (global) does not allow tags
    * use PARAM_RAW instead of PARAM_TEXT
    * fix language string typo in description

--- lang/en/bigbluebuttonbn.php
+++ lang/en/bigbluebuttonbn.php
@@ -226,7 +226,7 @@ $string['config_muteonstart_default_description'] = 'If enabled the session will
 $string['config_muteonstart_editable'] = 'Mute on start can be edited';
 $string['config_muteonstart_editable_description'] = 'Mute on start by default can be edited when the instance is added or updated.';
 $string['config_welcome_default'] = 'Default welcome message';
-$string['config_welcome_default_description'] = 'Replaces the default message setted up for the BigBlueButton server. The message can includes keywords  (%%CONFNAME%%, %%DIALNUM%%, %%CONFNUM%%) which will be substituted automatically, and also html tags like <b>...</b> or <i></i> ';
+$string['config_welcome_default_description'] = 'Replaces the default message set up for the BigBlueButton server. The message can includes keywords  (%%CONFNAME%%, %%DIALNUM%%, %%CONFNUM%%) which will be substituted automatically, and also html tags like <b>...</b> or <i></i> ';
 $string['config_default_messages'] = 'Default messages';
 $string['config_default_messages_description'] = 'Set message defaults for activities';
 
