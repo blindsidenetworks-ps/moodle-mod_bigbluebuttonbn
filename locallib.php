8484664a9864cc5486c113ccd0c0ef61cf1156e6
jfederico <jesus@123it.ca>
2021-08-20 00:06:40 +0000

    Removed forced validation when recording is protected

--- locallib.php
+++ locallib.php
@@ -1712,7 +1712,8 @@ function bigbluebuttonbn_get_recording_data_row_type($recording, $bbbsession, $p
         'data-target' => $playback['type'],
         'data-href' => $href,
       );
-    if ($CFG->bigbluebuttonbn_recordings_validate_url && !bigbluebuttonbn_is_bn_server()
+    if (!isset($recording['protected'])
+            && $CFG->bigbluebuttonbn_recordings_validate_url
             && !bigbluebuttonbn_is_valid_resource(trim($playback['url']))) {
         $linkattributes['class'] = 'btn btn-sm btn-warning';
         $linkattributes['title'] = get_string('view_recording_format_errror_unreachable', 'bigbluebuttonbn');
