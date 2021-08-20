c1f8c8aa67ae6636dee906d319336630b0eca6a7
jfederico <jesus@123it.ca>
2021-08-20 00:18:53 +0000

    v2.4.4: bumped up version

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2019101010;
+$plugin->version = 2019101011;
 $plugin->requires = 2016120500;
 $plugin->cron = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.4.3';
+$plugin->release = '2.4.4';
