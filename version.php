af006eb467ef44bbee130e4e72dd1f5cd12dc0a1
jfederico <jesus@123it.ca>
2021-08-26 20:51:28 +0000

    updated version tag

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2021072907.5;
+$plugin->version = 2021072907.7;
 $plugin->requires = 2020061500;
 $plugin->cron = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
