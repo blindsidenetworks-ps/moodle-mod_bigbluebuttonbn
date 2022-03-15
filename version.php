65e32ec349ca382b77cbb28a86249de077e4b74e
Jesus Federico <jesus@123it.ca>
2022-03-15 09:17:39 -0400

    Update version.php

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 
-$plugin->version = 2021101001.1;
+$plugin->version = 2021101002;
 $plugin->requires = 2020061500;
 $plugin->cron = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
