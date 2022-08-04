e28eb368caa828bb9015cd4b37c7a71d984f250b
jfederico <jesus@123it.ca>
2022-08-04 18:49:44 +0000

    3.0: release

--- version.php
+++ version.php
@@ -26,11 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-
-$plugin->version = 2021101007;
+$plugin->version = 2021101008;
 $plugin->requires = 2020061500;
 $plugin->cron = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
-$plugin->maturity = MATURITY_RC;
-$plugin->release = '3.0-rc.4';
-
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '3.0';
