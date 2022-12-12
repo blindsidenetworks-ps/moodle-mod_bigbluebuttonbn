4d6004ee96fb83dd70bd3add84e40b2c8d176041
Shamiso.Jaravaza <33659194+ssj365@users.noreply.github.com>
2022-12-12 11:30:53 -0700

    2.4.8-release (#597)

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2019101014;
+$plugin->version = 2019101015;
 $plugin->requires = 2016120500;
 $plugin->cron = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.4.7';
+$plugin->release = '2.4.8';
