65d65dc4692bc4016d5df96b9e824c1146c33749
Shamiso.Jaravaza <33659194+ssj365@users.noreply.github.com>
2024-04-10 15:28:36 -0600

    3.0.7-release

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2021101014;
+$plugin->version = 2021101015;
 $plugin->requires = 2020061500;
 $plugin->cron = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.0.6';
+$plugin->release = '3.0.7';
